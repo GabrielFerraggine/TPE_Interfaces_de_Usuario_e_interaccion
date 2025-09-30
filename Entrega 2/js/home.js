1b7dc668093b8aba8787df7d81a2fdc402e5b209
Santiago Andiarena <santiagoandiarena@gmail.com>
2025-09-30 00:45:30 -0300

    menu hamburguesa y user desplegado con animaciones simples. Carrusel principal con animacion

--- Entrega 2/js/home.js
+++ Entrega 2/js/home.js
@@ -253,4 +253,47 @@ document.addEventListener('DOMContentLoaded', initCarousel);
 init();
 
 
+// boton user
+
+const userAvatar = document.getElementById("userAvatar");
+const dropdownMenu = document.getElementById("dropdownMenu");
+const closeBtn = document.querySelector(".close-btn");
+
+// Mostrar / ocultar menú al clickear el avatar
+userAvatar.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("hidden");
+});
+
+// Botón "Volver" cierra el menú
+closeBtn.addEventListener("click", () => {
+  dropdownMenu.classList.add("hidden");
+});
+
+// Cierra si se hace clic afuera
+document.addEventListener("click", (event) => {
+  if (!dropdownMenu.contains(event.target) && !userAvatar.contains(event.target)) {
+    dropdownMenu.classList.add("hidden");
+  }
+});
+
+
+//boton menu hamburguesa
+
+const sidebar = document.getElementById("sidebar");
+const menuBtn = document.getElementById("menuHamburguesaBtn");
+const menuIcon = document.getElementById("menuIcon");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("expanded");
+
+  if (sidebar.classList.contains("expanded")) {
+    menuIcon.src = "img/close.png"; // icono de cerrar
+  } else {
+    menuIcon.src = "img/Menu Hamburguesa.png"; // icono hamburguesa
+    menuIcon.alt = "Abrir menú";
+  }
+});
+
+
+
 
